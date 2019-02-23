a839437d7971095d0f2654a30d464850107e59db
MammothStack <39247165+MammothStack@users.noreply.github.com>
2019-02-23 01:24:54 +0100

    Update boardinformation_unittest.py

--- tests/boardinformation_unittest.py
+++ tests/boardinformation_unittest.py
@@ -2,7 +2,7 @@ from src import board_information
 
 class TestInit(unittest.TestCase):
     def test_list(self):
-        self.assertRaises(ValueError, BoardInformation("red", 10000)
+        self.assertRaises(ValueError, BoardInformation("red", 10000))
 
 
 if __name__ == "__main__":
