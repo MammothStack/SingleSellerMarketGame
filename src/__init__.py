c77405e8646518bd66c3b9f14da8a61e135eaa27
MammothStack <39247165+MammothStack@users.noreply.github.com>
2019-02-23 02:58:16 +0100

    Changed imports again because im dumb

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,3 @@
-import pandas as pd
-import numpy as np
+from board_information import BoardInformation
+from player import Player
+from board_controller import BoardController
--- tests/test_board_information.py
+++ tests/test_board_information.py
@@ -1,4 +1,4 @@
-from src.board_information import BoardInformation
+from src import BoardInformation
 
 import unittest
 
