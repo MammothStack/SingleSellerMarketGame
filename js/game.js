805eafa1acfd5053776349450bf544957e9963c8
MammothStack <39247165+MammothStack@users.noreply.github.com>
2019-03-02 00:47:15 +0100

    modelling the game in js

---
+++ js/game.js
@@ -0,0 +1,51 @@
+class Player {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+class OperationModel {
+  constructor(
+    model,
+    name,
+    operation,
+    loss,
+    true_threshold,
+    max_cash_limit,
+    optimizer="adam",
+    metrics=["accuracy"]) {
+
+    if (optimizer === undefined) {
+      optimizer = "adam";
+    }
+
+    if (metrics === undefined) {
+      metrics=["accuracy"];
+    }
+
+    this.model = model;
+    this.name = name;
+    this.operation = operation;
+    this.loss = loss;
+    this.true_threshold = true_threshold;
+    this.max_cash_limit = max_cash_limit;
+    this.optimizer = optimizer;
+    this.metrics = metrics;
+  }
+}
+
+class BoardInformation {
+  constructor(player_names, max_cash_limit, houses, hotels) {
+    this.player_names = player_names;
+    this.max_cash_limit = max_cash_limit;
+    this.available_houses = houses;
+    this.available_hotels = hotels;
+  }
+}
+
+class BoardController {
+  constructor(operation_models, upgrade_limit) {
+    this.operation_models = operation_models;
+    this.upgrade_limit = upgrade_limit;
+  }
+}
